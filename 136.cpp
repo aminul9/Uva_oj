ad07e5ff91380971babd71fed2b27fb3da6d3d42
aminul9 <aminultpi34@gmail.com>
2018-02-27 14:54:44 +0600

    Add files via upload

---
+++ 136.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+//    long long  ans=11;
+//    for(long long i=16;;i++)
+//    {
+//        long long m=i;
+//        while(m!=0)
+//        {
+//            if(m%2==0 ||m%3==0|| m%5==0 )
+//            {
+//                if(m%2==0) m=m/2;
+//                if(m%3==0) m=m/3;
+//                if(m%5==0) m=m/5;
+//            }
+//            else break;
+//        }
+//
+//
+//        if(m==1)
+//            {
+//                ans++;
+//                cout<<i<<endl;
+//            }
+//
+//        if(ans==1500) {
+//                cout<<"The 1500'th ugly number is "<<i<<".";
+//                return 0;
+//        }
+//    }
+
+       cout<<"The 1500'th ugly number is 859963392.\n";
+
+
+    return 0;
+}
